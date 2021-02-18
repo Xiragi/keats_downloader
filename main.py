6514e502518214bc8279a6cb8c8164c943352cca
Chirag mahesh <chiragmahesh223@gmail.com>
2021-02-18 21:31:06 +0000

    fixed formatting

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import downloader
 import threading
 import time
-#Setup stage
 
-###Database
+# Setup stage
+
+# Database
 print("Would to like to create a new database?(Y/N)")
 print("If this is the first time running this then type in Y")
 response = input("")
@@ -16,47 +17,51 @@ else:
 
 global finalStep
 finalStep = False
+
+
 def aSyncVideoDownload():
     global FinalStep
     print("\nasync video download thread started\n")
-    databaseHandler2 = downloader.Database()
-    database2 = databaseHandler2.loadDatabase()
-    videoDownloader = downloader.VideoSaver(False)
+    database_handler2 = downloader.Database()
+    database2 = database_handler2.loadDatabase()
+    video_downloader = downloader.VideoSaver(False)
     while not finalStep:
         time.sleep(2)
-        videoDownloader.downloadVideo(database2)
+        video_downloader.downloadVideo(database2)
     print("\nasync video download thread ended\n")
-a = threading.Thread(target = aSyncVideoDownload) 
+
+
+a = threading.Thread(target=aSyncVideoDownload)
 a.start()
-      
-###Login
+
+# Login
 print("Have you logged in before? (Y/N)")
 response = input("")
 Browser = downloader.Selenium()
 if response == "N":
     print("Once you are logged in close the browser and wait")
     Browser.loadKeatsPage(False)
-    
-print("Setup stage complete")
-#Video Setup
 
-###Listing videos
+print("Setup stage complete")
+# Video Setup
+
+# Listing videos
 print("Trying to load courses.txt file")
 try:
     courses = downloader.Courses().getCourses()
 except:
-    print("Something went wrong opening courses.txt. Make sure that text file exists with your module link on each line")
+    print(
+        "Something went wrong opening courses.txt. Make sure that text file exists with your module link on each line")
 
 print("Getting the video list")
-Browser.getVideoList(database,courses)
+Browser.getVideoList(database, courses)
 print("Getting video URLs")
-delay = 1.0 #Increase this to 1.0 if you get a lot of "Failed to find frame"
+delay = 1.0  # Increase this to 1.0 if you get a lot of "Failed to find frame"
 
-Browser.getVideoURLs(database,delay)
+Browser.getVideoURLs(database, delay)
 
-
-#Video downloading
-###Downloading videos
+# Video downloading
+# Downloading videos
 finalStep = True
 print("Downloading videos")
 videoDownloader = downloader.VideoSaver(False)
